5f351f0158021bae32a1978f6a1cbce7d069dcf0
Lukasz Burak <lukasz.burak@inspeerity.com>
2019-11-28 20:13:31 +0100

    Modify App entry point to support remote mode.

--- src/main/java/com/staxter/player/PlayerApplication.java
+++ src/main/java/com/staxter/player/PlayerApplication.java
@@ -1,10 +1,28 @@
 package com.staxter.player;
 
 import com.staxter.player.local.LocalApplication;
+import com.staxter.player.network.RemoteApplication;
+
+import java.util.Arrays;
 
 public class PlayerApplication {
 
+    private static final String REMOTE_APP_ARG = "remote";
+    private static final String REMOTE_APP_INITIATOR_ARG = "-i";
+
     public static void main( String[] args ) {
-        LocalApplication.start();
+        if (isRemoteVersion(args)) {
+            RemoteApplication.start(isInitiator(args));
+        } else {
+            LocalApplication.start();
+        }
+    }
+
+    private static boolean isRemoteVersion(String[] args) {
+        return Arrays.asList(args).contains(REMOTE_APP_ARG);
+    }
+
+    private static boolean isInitiator(String[] args) {
+        return Arrays.asList(args).contains(REMOTE_APP_INITIATOR_ARG);
     }
 }
