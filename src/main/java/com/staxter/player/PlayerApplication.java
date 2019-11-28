b8bdc22d6f29c7da9deccf90c2c3665e49dca1ee
Lukasz Burak <lukasz.burak@inspeerity.com>
2019-11-28 19:56:14 +0100

    Implement Entry point to the app.

---
+++ src/main/java/com/staxter/player/PlayerApplication.java
@@ -0,0 +1,10 @@
+package com.staxter.player;
+
+import com.staxter.player.local.LocalApplication;
+
+public class PlayerApplication {
+
+    public static void main( String[] args ) {
+        LocalApplication.start();
+    }
+}
